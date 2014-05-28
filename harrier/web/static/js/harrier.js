6f6aacaa60d7c771bcca0d4f4e053ed70cbe472c
Andrew E. Bruno <aebruno2@buffalo.edu>
2014-05-28 16:06:01 -0400

    Store relative x,y coords. Support for dynamic size canvas

--- harrier/web/static/js/harrier.js
+++ harrier/web/static/js/harrier.js
@@ -1,11 +1,32 @@
 var Harrier = function() {
     var index = 0;
     var images = [];
-    var width = 632;
-    var height = 504;
+    var width = 432;
+    var height = 304;
+    var canvas = null;
+    var ctx = null;
 
     function init(id, curi) {
         index = curi;
+        canvas = document.getElementById('drop');
+        ctx = canvas.getContext('2d');
+        setWidth(width, height);
+
+        canvas.addEventListener("mousedown", function(evt) {
+            var pos = getPosition(evt);
+
+            drawCircle(pos.x,pos.y);
+            var x = pos.x / width;
+            var y = pos.y / height;
+            $.ajax({
+                url: '/data/image/'+images[index]+'/target/add',
+                type: "POST",
+                data: {'x': x, 'y': y},
+                success: function(data) {
+                }
+            });
+        }, false);
+
         $.ajax({
             url: '/data/imageset/'+id,
             type: "GET",
@@ -22,6 +43,11 @@ var Harrier = function() {
                 $('#save-cat').click(saveCategory);
             }
         });
+    }
+
+    function setWidth(w, h) {
+        canvas.width = w;
+        canvas.height = h;
     }
 
     function next() {
@@ -79,20 +105,17 @@ var Harrier = function() {
 
     function displayImage(idata) {
         $("#image-slider .caption").width(width);
-        var canvas = document.getElementById('drop');
-        canvas.width = width;
-        canvas.height = height;
-        canvas.addEventListener("mousedown", getPosition, false);
-        var ctx = canvas.getContext('2d');
         ctx.clearRect(0, 0, width, height);
         var img = new Image();
         img.src = idata.url;
         img.width = width;
         img.height = height;
         img.onload = function(){
-            ctx.drawImage(img,0,0, img.width, img.height);
+            ctx.drawImage(img, 0, 0, img.width, img.height);
             $.each(idata.targets, function(index, t) {
-                drawCircle(t.x,t.y);
+                var x = t.x * width;
+                var y = t.y * height;
+                drawCircle(x,y);
             });
         }
 
@@ -108,43 +131,19 @@ var Harrier = function() {
     }
 
     function drawCircle(x, y) {
-        var canvas = document.getElementById('drop');
-        var ctx = canvas.getContext('2d');
         ctx.fillStyle = "rgba(255, 0, 0, .3)";
         ctx.beginPath();
-        ctx.arc(x, y, 15, 0, Math.PI*2, true);
+        ctx.arc(x, y, Math.round(width*0.02), 0, Math.PI*2, true);
         ctx.closePath();
         ctx.fill();
     }
 
-    // http://miloq.blogspot.com/2011/05/coordinates-mouse-click-canvas.html
-    function getPosition(event) {
-        var x = new Number();
-        var y = new Number();
-        var canvas = document.getElementById("drop");
+    function getPosition(evt) {
+        var rect = canvas.getBoundingClientRect();
+        var x = evt.clientX - rect.left;
+        var y = evt.clientY - rect.top;
 
-        if (event.x != undefined && event.y != undefined) {
-            x = event.x;
-            y = event.y;
-        }
-        else  { // Firefox method to get the position
-            x = event.clientX + document.body.scrollLeft +
-                document.documentElement.scrollLeft;
-            y = event.clientY + document.body.scrollTop +
-                document.documentElement.scrollTop;
-        }
-
-        x -= canvas.offsetLeft;
-        y -= canvas.offsetTop;
-
-        drawCircle(x,y);
-        $.ajax({
-            url: '/data/image/'+images[index]+'/target/add',
-            type: "POST",
-            data: {'x': x, 'y': y},
-            success: function(data) {
-            }
-        });
+        return {'x': x, 'y': y};
     }
 
     return {
