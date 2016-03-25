b5425ae09bda18a8a72f67596e7ccf7c489244ea
Andrew E. Bruno <aebruno2@buffalo.edu>
2016-03-25 12:15:39 -0400

    Update for use with uwsgi

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
-from werkzeug.serving import run_simple
-from werkzeug.wsgi import DispatcherMiddleware
+#from werkzeug.serving import run_simple
+#from werkzeug.wsgi import DispatcherMiddleware
 
 from harrier import web
 
-application = DispatcherMiddleware(web.create_app())
+#application = DispatcherMiddleware(web.create_app())
+application = web.create_app()
 
 if __name__ == "__main__":
-    run_simple('127.0.0.1', 5000, application, use_reloader=True, use_debugger=True)
+    application.run()
+#    run_simple('127.0.0.1', 5000, application, use_reloader=True, use_debugger=True)
